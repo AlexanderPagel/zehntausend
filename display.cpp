4f0ef932bdfdb6534219325b1662881d3d01278d
Alexander Pagel <Alexander.Pagel@rub.de>
2020-12-30 20:32:53 +0100

    Move display layout comment to header

--- display.cpp
+++ display.cpp
@@ -6,21 +6,6 @@
 namespace ui
 {
 
-// Preliminary screen setup:
-
-/*
-Player 1:    50                                                                  3 player lines
-Player 2:   300                                                                  .
-Player 3: 10000                                                                  .
-                                                                                 1 empty line
-     |    Dice:    1   2   3   4   5   6                                         1 never changing default line
-   0 |                                                                           1 line just with player number
-     |      0  -> [3] [3] [3] [6] [1] [5]                                        1 line with rolled dice,
-                                                                                             currernt points,
-                                                                                             active indivcators, and
-                                                                                             yet-to-move indicators
-*/
-
 void
 Ui::drawPlayer(Ui::Game_t::Player p)
 {
