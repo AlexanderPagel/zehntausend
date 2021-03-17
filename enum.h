cdaa99d34a067b911271d6c2c2316ec155dd0b5c
Alexander Pagel <Alexander.Pagel@rub.de>
2021-03-17 04:14:09 +0100

    Add some enum generics

---
+++ enum.h
@@ -0,0 +1,39 @@
+// enum.h
+//
+// Some generic enum utils. I like enums (maybe I should not).
+//
+// Utils for "canonical enums" := Enums that define, additionally to any other
+// values:
+//  - begin := the smallest legal value
+//  - end   := largest legal value + 1
+//  - none  := A (magic) legal value indicating that the value is not yet known
+
+// Hard cast to underlying type, user needs to care for correctness. This is
+// used explicitly to disallow all non-explicit operations between different
+// enum types.
+
+
+#include <cassert>
+#include <type_traits>
+
+
+template<typename Enum,
+         typename = std::enable_if_t<std::is_enum<Enum>::value>
+        >
+inline constexpr auto
+raw(Enum e) -> std::underlying_type_t<Enum>
+{
+  return static_cast<std::underlying_type_t<Enum>>(e);
+}
+
+template<typename Enum,
+         typename = std::enable_if_t<std::is_enum<Enum>::value>
+        >
+inline constexpr auto
+legit(Enum e) -> bool
+{
+//  using Base_t = typename std::underlying_type_t<Enum>;
+  assert(  e == Enum::none
+        || ((Enum::begin) <= (e) && (e) < (Enum::end)));
+  return e != Enum::none;
+}
