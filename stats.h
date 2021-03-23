f10c17fbb14df5da559c7058a2cf0a0d623c7c55
Alexander Pagel <Alexander.Pagel@rub.de>
2021-03-23 23:42:24 +0100

    save before rewriting offset version

--- stats.h
+++ stats.h
@@ -1,6 +1,29 @@
 // stats.h
 //
-// TODO Brief explanation.
+// Implements a mean and variance computation.
+// Wikipedia [https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance]:
+//
+//  # For a new value newValue, compute the new count, new mean, the new M2.
+//  # mean accumulates the mean of the entire dataset
+//  # M2 aggregates the squared distance from the mean
+//  # count aggregates the number of samples seen so far
+//  def update(existingAggregate, newValue):
+//      (count, mean, M2) = existingAggregate
+//      count += 1
+//      delta = newValue - mean
+//      mean += delta / count
+//      delta2 = newValue - mean
+//      M2 += delta * delta2
+//      return (count, mean, M2)
+
+//  # Retrieve the mean, variance and sample variance from an aggregate
+//  def finalize(existingAggregate):
+//      (count, mean, M2) = existingAggregate
+//      if count < 2:
+//          return float("nan")
+//      else:
+//          (mean, variance, sampleVariance) = (mean, M2 / count, M2 / (count - 1))
+//          return (mean, variance, sampleVariance)
 
 
 #ifndef STATS_H_INCLUDED
@@ -30,11 +53,7 @@ class Mean
     Value_type sum{0};
     Long_type n{0};
 
-    Mean_type computeMean() const
-    {
-      return getN() > 0 ? static_cast<Mean_type>(sum) / static_cast<Mean_type>(n)
-                        : 0;
-    }
+    Mean_type computeMean() const;
 
   public:
     Value_type getSum() const { return sum; }
@@ -50,7 +69,7 @@ class Mean
 };
 
 template<typename T, typename M = double>
-class Stats : public Mean<T, M>
+class Stats : protected Mean<T, M>
 {
   private:
     using Base_type = Mean<T,M>;
@@ -64,17 +83,16 @@ class Stats : public Mean<T, M>
 
   private:
     Variance_type varianceCumulative {0};
+    // TODO Should bias have Mean_type?
+    Value_type const bias{0}; // Fixed bias for variance computation
 
-    Mean_type offsetOf(T t) const;
+    Mean_type offsetFromMean(Value_type v) const;
+    Value_type biasedValue(Value_type v) const;
 
-    void updateVariance(T t)
-    {
-      Mean_type const offset {offsetOf(t)};
-      Base_type::addToMean(t);
-      varianceCumulative = varianceCumulative + offset * (t - getMean());
-    }
+    void updateVariance(T t);
 
   public:
+    explicit Stats(Value_type bias) : bias(bias) {}
     using Base_type::getN;
     using Base_type::getMean;
     bool hasVariance() const { return getN() > 1; }
@@ -86,10 +104,9 @@ class Stats : public Mean<T, M>
                         : -1;
     }
     StdDeviation_type getStdDeviation() const { return getN() > 0 ? std::sqrt(getVariance()) : -1; }
-    std::pair<Mean_type, Mean_type> operator()() const
-    {
-      return std::make_pair(getMean(), getStdDeviation());
-    }
+    std::pair<Mean_type, Mean_type> operator()() const;
+    auto operator+=()(Value_type v);
+    auto addToStats(Value_type v);
 };
 
 } // namespace stats
