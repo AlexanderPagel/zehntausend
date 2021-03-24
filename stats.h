1cc4b556f171cbbcea21dfedf1f5150105d270c3
Alexander Pagel <Alexander.Pagel@rub.de>
2021-03-24 01:07:31 +0100

    Combine stats into single class "Stats"

--- stats.h
+++ stats.h
@@ -1,8 +1,13 @@
 // stats.h
 //
-// Implements a mean and variance computation.
-// Wikipedia [https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance]:
+// Implements a mean and variance computation. Contrary to the algorithms
+// below, we compute the average exactly by summizing over all values. We use
+// these stats for RL algorithm evaluation and returns are typically in range
+// [0, 2000].
+
+// References:
 //
+// Wikipedia [https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance]:
 //  # For a new value newValue, compute the new count, new mean, the new M2.
 //  # mean accumulates the mean of the entire dataset
 //  # M2 aggregates the squared distance from the mean
@@ -15,7 +20,7 @@
 //      delta2 = newValue - mean
 //      M2 += delta * delta2
 //      return (count, mean, M2)
-
+//
 //  # Retrieve the mean, variance and sample variance from an aggregate
 //  def finalize(existingAggregate):
 //      (count, mean, M2) = existingAggregate
@@ -24,10 +29,22 @@
 //      else:
 //          (mean, variance, sampleVariance) = (mean, M2 / count, M2 / (count - 1))
 //          return (mean, variance, sampleVariance)
+//
+// Maybe implement removal operation later.
+// [https://stackoverflow.com/questions/30876298/removing-a-prior-sample-while-using-welfords-method-for-computing-single-pass-v]:
+//  Mk-1 = Mk - (xk - Mk) / (k - 1).
+//  Sk-1 = Sk - (xk – Mk-1) * (xk – Mk).
+// Looks stable enough to me since the changes look very similar in size to the
+// adding terms. Rouding problems should be about same size as for adding case.
+// I only intent to use for runnign average anyway, so low noise should not be
+// able to accumulate.
 
 
 #ifndef STATS_H_INCLUDED
 #define STATS_H_INCLUDED 1
+
+
+#include <tuple>
 
 
 namespace stats
@@ -38,75 +55,56 @@ namespace stats
 template<typename T, typename M = double,
          typename = std::enable_if_t<std::is_floating_point_v<M>>
         >
-class Mean
+class Stats
 {
   public:
     using Long_type = uint64_t;
-    using Mean_type = M; // M: Floating point type used for computations
     using Value_type = T; // T: Type we want to aggregate and compute the mean over
+    using Mean_type = M; // M: Floating point type used for computations
+    using StdDeviation_type = Variance_type;
+    using Variance_type = Mean_type;
     // Not sure how much is actually needed, but better not take the 32 bit bet
     static_assert(sizeof(Value_type) >= 8);
     static_assert(sizeof(Long_type) >= 4); // 32 bit should be enough for counting only
     static_assert(sizeof(Mean_type) >= 4); // TODO Makes sense for floats?
 
   protected:
-    Value_type sum{0};
     Long_type n{0};
-
-    Mean_type computeMean() const;
-
-  public:
-    Value_type getSum() const { return sum; }
-    Long_type getN() const { return n; }
-    bool hasMean() const { return getN() > 0; }
-
-    // Easiert to use from outside
-    Mean_type operator()() const { computeMean(); }
-    Mean& operator+=(Value_type t) { sum += t; ++n; return *this; }
-    // Easier to type in derived classes
-    auto getMean() const { return this->oeprator()(); }
-    auto addToMean(Value_type t) { return this->operator+=(t); }
-};
-
-template<typename T, typename M = double>
-class Stats : protected Mean<T, M>
-{
-  private:
-    using Base_type = Mean<T,M>;
-
-  public:
-    using Long_type = typename Base_type::Long_type;
-    using Mean_type = typename Base_type::Mean_type;
-    using Value_type = typename Base_type::Value_type;
-    using Variance_type = Mean_type;
-    using StdDeviation_type = Variance_type;
-
-  private:
-    Variance_type varianceCumulative {0};
-    // TODO Should bias have Mean_type?
-    Value_type const bias{0}; // Fixed bias for variance computation
+    Value_type sum{0};
+    Variance_type squareSum { 0 };
 
     Mean_type offsetFromMean(Value_type v) const;
-    Value_type biasedValue(Value_type v) const;
+    Variance_type varFinalize(bool sample) const;
+    StdDeviation_type stdDeviationFinalize(bool sample) const;
 
-    void updateVariance(T t);
+
+    // Add *only* to the mean parts 'n' and 'sum'
+    void addToMean(Value_type t);
+    // Update mean *and* variance parts
+    void updateVarianceAndMean(T t);
 
   public:
-    explicit Stats(Value_type bias) : bias(bias) {}
-    using Base_type::getN;
-    using Base_type::getMean;
+    bool hasMean() const { return getN() > 0; }
     bool hasVariance() const { return getN() > 1; }
-    bool gasStdDeviation() const { return hasVariance(); }
-    // Sanitize std::abs in case inaccurate float operations produce negative
-    Variance_type getVariance() const
-    {
-      return getN() > 0 ? std::abs(varianceCumulative / static_cast<Variance_type>(getN()))
-                        : -1;
-    }
-    StdDeviation_type getStdDeviation() const { return getN() > 0 ? std::sqrt(getVariance()) : -1; }
-    std::pair<Mean_type, Mean_type> operator()() const;
-    auto operator+=()(Value_type v);
-    auto addToStats(Value_type v);
+    bool hasStdDeviation() const { return hasVariance(); }
+
+    Long_type getN() const { return n; }
+    Value_type getSum() const { return sum; }
+    Mean_type getMean() const;
+    Variance_type getVariance() const;
+    Variance_type getSampleVariance() const;
+    StdDeviation_type getStdDeviation() const;
+    StdDeviation_type getSampleStdDeviation() const;
+
+    // Get (N, sum, mean, var, o, sample_var, sample_o)
+    using StatRecord
+      = std::tuple<Long_type, Value_type, Mean_type, Variance_type,
+                   StdDeviation_type, Variance_type, StdDeviation_type>;
+    StatRecord operator()() const;
+
+    // Easiert to use from outside
+    Stats& operator+=(Value_type t);
+    // Easier to type in derived classes
 };
 
 } // namespace stats
